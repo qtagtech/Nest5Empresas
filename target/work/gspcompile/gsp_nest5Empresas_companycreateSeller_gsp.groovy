11df2ba1f316759378092665de1541084b8e803f
Juan David Arroyave <juan.arroyave@qtagtech.com>
2014-09-10 10:02:39 -0500

    nada ¿?

---
+++ target/work/gspcompile/gsp_nest5Empresas_companycreateSeller_gsp.groovy
@@ -0,0 +1,77 @@
+import org.codehaus.groovy.grails.plugins.metadata.GrailsPlugin
+import org.codehaus.groovy.grails.web.pages.GroovyPage
+import org.codehaus.groovy.grails.web.taglib.*
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
+import org.springframework.web.util.*
+import grails.util.GrailsUtil
+
+class gsp_nest5Empresas_companycreateSeller_gsp extends GroovyPage {
+public String getGroovyPageFileName() { "/WEB-INF/grails-app/views/company/createSeller.gsp" }
+public Object run() {
+Writer out = getOut()
+Writer expressionOut = getExpressionOut()
+registerSitemeshPreprocessMode()
+printHtmlPart(0)
+createTagBody(1, {->
+printHtmlPart(1)
+invokeTag('captureMeta','sitemesh',4,['gsp_sm_xmlClosingForEmptyTag':("/"),'name':("layout"),'content':("dashboardLayout")],-1)
+printHtmlPart(1)
+invokeTag('captureMeta','sitemesh',5,['gsp_sm_xmlClosingForEmptyTag':("/"),'http-equiv':("Content-Type"),'content':("text/html; charset=utf-8")],-1)
+printHtmlPart(1)
+createTagBody(2, {->
+createClosureForHtmlPart(2, 3)
+invokeTag('captureTitle','sitemesh',6,[:],3)
+})
+invokeTag('wrapTitleTag','sitemesh',6,[:],2)
+printHtmlPart(1)
+invokeTag('captureMeta','sitemesh',7,['gsp_sm_xmlClosingForEmptyTag':("/"),'name':("author"),'content':("Nest5.com")],-1)
+printHtmlPart(1)
+invokeTag('captureMeta','sitemesh',8,['gsp_sm_xmlClosingForEmptyTag':("/"),'name':("description"),'content':("")],-1)
+printHtmlPart(1)
+invokeTag('captureMeta','sitemesh',9,['gsp_sm_xmlClosingForEmptyTag':("/"),'name':("keywords"),'content':("")],-1)
+printHtmlPart(1)
+invokeTag('captureMeta','sitemesh',10,['gsp_sm_xmlClosingForEmptyTag':("/"),'name':("application-name"),'content':("Nest5")],-1)
+printHtmlPart(3)
+invokeTag('captureMeta','sitemesh',13,['gsp_sm_xmlClosingForEmptyTag':("/"),'name':("viewport"),'content':("width=device-width, initial-scale=1.0")],-1)
+printHtmlPart(4)
+invokeTag('captureMeta','sitemesh',15,['gsp_sm_xmlClosingForEmptyTag':("/"),'http-equiv':("X-UA-Compatible"),'content':("IE=edge")],-1)
+printHtmlPart(5)
+invokeTag('require','r',18,['modules':("dashboard,uniform")],-1)
+printHtmlPart(1)
+})
+invokeTag('captureHead','sitemesh',19,[:],1)
+printHtmlPart(6)
+createTagBody(1, {->
+printHtmlPart(7)
+expressionOut.print(youarehere)
+printHtmlPart(8)
+expressionOut.print(createLink(controller: 'seller', action: 'saveFromCompany'))
+printHtmlPart(9)
+if(true && (flash.message)) {
+printHtmlPart(10)
+expressionOut.print(flash.message)
+printHtmlPart(11)
+}
+printHtmlPart(12)
+createTagBody(2, {->
+printHtmlPart(13)
+expressionOut.print(createLink(controller: 'seller', action: 'saveFromCompany'))
+printHtmlPart(14)
+})
+invokeTag('script','r',169,[:],2)
+printHtmlPart(15)
+})
+invokeTag('captureBody','sitemesh',173,[:],1)
+printHtmlPart(16)
+}
+public static final Map JSP_TAGS = new HashMap()
+protected void init() {
+	this.jspTags = JSP_TAGS
+}
+public static final String CONTENT_TYPE = 'text/html;charset=UTF-8'
+public static final long LAST_MODIFIED = 1409609987093L
+public static final String EXPRESSION_CODEC = 'html'
+public static final String STATIC_CODEC = 'none'
+public static final String OUT_CODEC = 'html'
+public static final String TAGLIB_CODEC = 'none'
+}
